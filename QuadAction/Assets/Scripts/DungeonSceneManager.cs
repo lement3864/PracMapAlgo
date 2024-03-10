194abb7caadf3db64224886d7a8240526301cce9
신현기 <muffin3864@naver.com>
2024-03-11 07:43:31 +0900

    240311 FEAT : morning add

--- QuadAction/Assets/Scripts/DungeonSceneManager.cs
+++ QuadAction/Assets/Scripts/DungeonSceneManager.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Text;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -17,6 +19,19 @@ public class DungeonSceneManager : MonoBehaviour
 
     public RoomType[,] dungeonMap;
     public Vector3 entrancePosition;
+    public RoomConnection[,] connections;
+
+    List<Vector2Int> path;
+
+    public class RoomConnection
+    {
+        public bool top, right, bottom, left;
+
+        public RoomConnection()
+        {
+            top = right = bottom = left = false;
+        }
+    }
 
     void Awake()
     {
@@ -30,7 +45,6 @@ public class DungeonSceneManager : MonoBehaviour
             Destroy(gameObject); // 중복 인스턴스 제거
         }
     }
-
 
     public void EnterDungeon()
     {
@@ -46,6 +60,7 @@ public class DungeonSceneManager : MonoBehaviour
     void GenerateDungeon()
     {
         dungeonMap = new RoomType[width, height];
+        connections = new RoomConnection[width, height];
 
         // 모든 방을 몬스터 방으로 초기화
         for (int x = 0; x < width; x++)
@@ -53,6 +68,7 @@ public class DungeonSceneManager : MonoBehaviour
             for (int y = 0; y < height; y++)
             {
                 dungeonMap[x, y] = RoomType.Monster;
+                connections[x, y] = new RoomConnection();
             }
         }
 
@@ -66,18 +82,14 @@ public class DungeonSceneManager : MonoBehaviour
         PlaceSpecialRoom(RoomType.Shop, specialRoomPositions);
         PlaceSpecialRoom(RoomType.Boss, specialRoomPositions);
 
-        for (int x = 0; x < width; x++)
-        {
-            for (int y = 0; y < height; y++)
-            {
-                if (dungeonMap[x, y] == RoomType.Entrance)
-                {
-                    // Entrance 위치 계산, 방의 실제 월드 위치를 entrancePosition에 저장
-                    entrancePosition = new Vector3(x * 500, 0, y * 500); // 간격을 500으로 설정했다고 가정
-                    break;
-                }
-            }
-        }
+        CreateGuaranteedPath();
+
+        PrintPath();
+
+        Vector2Int entrancePositionIndex = FindRoomPosition(RoomType.Entrance);
+        entrancePosition = new Vector3(entrancePositionIndex.x * 500, 0, entrancePositionIndex.y * 500);
+
+        PrintAllPortalConnections();
 
         // 방 출력 및 검사 (디버깅)
         for (int y = 0; y < height; y++)
@@ -102,5 +114,109 @@ public class DungeonSceneManager : MonoBehaviour
 
         dungeonMap[position.x, position.y] = roomType;
         occupiedPositions.Add(position);
+    }
+    void CreateGuaranteedPath()
+    {
+        Vector2Int entrancePos = FindRoomPosition(RoomType.Entrance);
+        Vector2Int bossPos = FindRoomPosition(RoomType.Boss);
+
+        Vector2Int currentPos = entrancePos;
+
+        path = new List<Vector2Int>(); // 클래스 레벨의 path 리스트를 초기화
+        path.Add(currentPos); // 입장방 위치를 경로에 추가
+
+        while (currentPos != bossPos)
+        {
+            // 간단한 경로 생성 로직 (여기서는 상하좌우 한 방향으로만 이동)
+            if (currentPos.x < bossPos.x)
+            {
+                connections[currentPos.x, currentPos.y].right = true;
+                currentPos.x++;
+            }
+            else if (currentPos.x > bossPos.x)
+            {
+                connections[currentPos.x, currentPos.y].left = true;
+                currentPos.x--;
+            }
+            else if (currentPos.y < bossPos.y)
+            {
+                connections[currentPos.x, currentPos.y].top = true;
+                currentPos.y++;
+            }
+            else if (currentPos.y > bossPos.y)
+            {
+                connections[currentPos.x, currentPos.y].bottom = true;
+                currentPos.y--;
+            }
+
+            path.Add(currentPos); // 현재 위치를 경로에 추가
+        }
+    }
+
+    Vector2Int FindRoomPosition(RoomType targetRoomType)
+    {
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                if (dungeonMap[x, y] == targetRoomType)
+                {
+                    return new Vector2Int(x, y);
+                }
+            }
+        }
+
+        // 만약 원하는 타입의 방을 찾지 못한 경우, 에러를 반환하거나 유효하지 않은 위치를 반환
+        Debug.LogError($"Room of type {targetRoomType} not found.");
+        return new Vector2Int(-1, -1); // 유효하지 않은 위치를 나타내는 값
+    }
+
+
+    void PrintPath()
+    {
+        if (path == null || path.Count == 0)
+        {
+            Debug.Log("No path data available to print.");
+            return;
+        }
+
+        StringBuilder pathString = new StringBuilder("Path from Entrance to Boss: ");
+        foreach (Vector2Int pos in path)
+        {
+            pathString.Append($"({pos.x}, {pos.y}) -> ");
+        }
+
+        // 마지막 화살표를 제거합니다.
+        pathString.Remove(pathString.Length - 4, 4);
+
+        Debug.Log(pathString.ToString());
+    }
+
+    void PrintAllPortalConnections()
+    {
+        StringBuilder sb = new StringBuilder();
+
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                sb.Clear();
+                RoomConnection connection = connections[x, y];
+                sb.AppendFormat("Room at ({0},{1}) connections: ", x, y);
+
+                if (connection.top) sb.Append("Top ");
+                if (connection.right) sb.Append("Right ");
+                if (connection.bottom) sb.Append("Bottom ");
+                if (connection.left) sb.Append("Left ");
+
+                // 방에 아무 연결도 없는 경우
+                if (!connection.top && !connection.right && !connection.bottom && !connection.left)
+                {
+                    sb.Append("None");
+                }
+
+                Debug.Log(sb.ToString());
+            }
+        }
     }
 }
